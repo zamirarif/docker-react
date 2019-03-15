8e15d62ea42dcaeffd57aa3ce82e8a3eed2da071
ZamirArif <zamir.arif@gmail.com>
2019-03-15 14:45:29 +0100

    Now we are playing with feature branch

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+           I was changed on the feature branch
           </a>
         </header>
       </div>
